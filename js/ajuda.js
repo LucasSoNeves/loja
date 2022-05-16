e8ed2d55426d46275de80ef7c2f8dcf6bdbeba1a
main <Lucas.neves.ead@gmail.com>
2022-05-16 09:28:12 -0300

    estilização da página de ajuda

---
+++ js/ajuda.js
@@ -0,0 +1,78 @@
+var btn = document.querySelector('#mostrar');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click', function() {
+    
+    if(container.style.display === 'block') {
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
+
+})
+
+
+var btn = document.querySelector('#mostrar1');
+var container1 = document.querySelector('.container1');
+
+btn.addEventListener('click', function() {
+    
+    if(container1.style.display === 'block') {
+        container1.style.display = 'none';
+    } else {
+        container1.style.display = 'block';
+    }
+
+})
+
+var btn = document.querySelector('#mostrar2');
+var container2 = document.querySelector('.container2');
+
+btn.addEventListener('click', function() {
+    
+    if(container2.style.display === 'block') {
+        container2.style.display = 'none';
+    } else {
+        container2.style.display = 'block';
+    }
+
+})
+
+var btn = document.querySelector('#mostrar3');
+var container3 = document.querySelector('.container3');
+
+btn.addEventListener('click', function() {
+    
+    if(container3.style.display === 'block') {
+        container3.style.display = 'none';
+    } else {
+        container3.style.display = 'block';
+    }
+
+})
+
+var btn = document.querySelector('#mostrar4');
+var container4 = document.querySelector('.container4');
+
+btn.addEventListener('click', function() {
+    
+    if(container4.style.display === 'block') {
+        container4.style.display = 'none';
+    } else {
+        container4.style.display = 'block';
+    }
+
+})
+
+var btn = document.querySelector('#mostrar5');
+var container5 = document.querySelector('.container5');
+
+btn.addEventListener('click', function() {
+    
+    if(container5.style.display === 'block') {
+        container5.style.display = 'none';
+    } else {
+        container5.style.display = 'block';
+    }
+
+})
